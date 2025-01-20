98f61cc3622ae056c8d91ca5daede2acd5b5fffb
Sergey Gusterev <copywriter-sg@yandex.ru>
2025-01-20 19:10:27 +0300

    feat: date-picker

--- src/app/_components/icons/index.ts
+++ src/app/_components/icons/index.ts
@@ -1,5 +1,15 @@
 import MobileIcon from './mobile';
 import BurgerIcon from './burger';
 import CloseIcon from './close';
+import DataPickerIcon from './datapicker';
+import ArrowLeftIcon from './arrow-left';
+import ArrowRightIcon from './arrow-right';
 
-export { MobileIcon, BurgerIcon, CloseIcon };
+export {
+  MobileIcon,
+  BurgerIcon,
+  CloseIcon,
+  DataPickerIcon,
+  ArrowLeftIcon,
+  ArrowRightIcon,
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import { dataHeroSlider } from './_shared/constans';
 import Slider from './_components/slider';
+import BookForm from './_components/book-form';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main>
-      <section className="container">
+      <section className={`container ${styles.hero}`}>
         <Slider data={dataHeroSlider} height={640} />
-        <Slider data={dataHeroSlider} height={520} onlyImage={true} />
+      </section>
+      <section className={`container ${styles.book}`}>
+        <BookForm />
       </section>
     </main>
   );
