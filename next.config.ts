ea7b49b43460c390a2896e95c96343dc26c4d21b
Sergey Gusterev <copywriter-sg@yandex.ru>
2025-01-22 15:33:19 +0300

    config nextjs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/bri-village-nextjs',
+  output: 'export',
+  reactStrictMode: true,
 };
 
 export default nextConfig;
